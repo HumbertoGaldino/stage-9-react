a3aa13c025a2e61ba3598b18c3e8b23543de0429
Humberto Galdino <humberto.galdino@live.com>
2024-05-02 22:43:24 -0300

    Update

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,31 @@
-import { Container } from './styles';
-import { Button } from '../../components/Button';
+import { Container, Links } from './styles'
+
+import { Header } from '../../components/Header'
+import { Button } from '../../components/Button'
+import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
 
 export function Details() {
   return (
     <Container>
-      <h1>Hello World!</h1>
-      <span>Rodrigo Gonçalves</span>
+      <Header />
 
-      <Button title="Login" loading />
-      <Button title="Cadastrar" />
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href="#">https://rocketseat.com.br</a>
+          </li>
+          <li>
+            <a href="#">https://rocketseat.com.br</a>
+          </li>
+        </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="node" />
+      </Section>
+
       <Button title="Voltar" />
     </Container>
   )
